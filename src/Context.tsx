129aaa76e7305580d7ae8ac7fef2d705e05adcfa
sergey <zhdanushkaa@yandex.ru>
2022-05-29 15:20:05 +0500

    Задание по Контексту

--- src/Context.tsx
+++ src/Context.tsx
@@ -1,10 +1,14 @@
-import { ReactElement } from 'react';
+import { ReactElement, createContext, useContext } from 'react';
 
 type Theme = 'light' | 'dark';
+const theme = createContext<Theme>('light');
 
-export function ThemeProvider(props: {
-    theme: Theme;
-    children: ReactElement;
-}) {}
+export function ThemeProvider(props: { theme: Theme; children: ReactElement }) {
+    return (
+        <theme.Provider value={props.theme}>{props.children}</theme.Provider>
+    );
+}
 
-export function useTheme(): Theme {}
+export function useTheme(): Theme {
+    return useContext(theme);
+}
